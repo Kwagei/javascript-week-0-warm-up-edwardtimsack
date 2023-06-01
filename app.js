63fca8f4ed7b8b774f6ef91fe12452959d589993
edwardtimsack <edwardtimothysackie@gmail.com>
2023-06-01 12:19:39 +0000

    completed assignment

---
+++ app.js
@@ -0,0 +1,26 @@
+// Problem 1 Solution
+var x = 10;
+var y = 3;
+x % y;
+
+// Problem 2 Solution
+var name = "Edward";
+    console.log(name);
+
+    // Problem 3 Solution
+    var num1 = 5;
+    var num2 = 10;
+    var area = num1 + num2;
+    console.log(area);
+
+
+// Problem 4 Solution
+var radius = 15;
+var area = Math.PI *Math.pow(radius, 2);
+console.log(area);
+
+
+// Problem 5 Solution
+var num = 15;
+num += 5;
+console.log(num);
